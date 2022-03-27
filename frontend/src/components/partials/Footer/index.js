75efbf9b45aaf430ed996b85bc399cfb3ee3a7c7
Marcos <lamberti.marcos049@gmail.com>
2022-03-27 18:56:28 -0300

    Criação do footer

--- frontend/src/components/partials/Footer/index.js
+++ frontend/src/components/partials/Footer/index.js
@@ -5,7 +5,11 @@ import { FooterArea } from './styled';
 const Footer = () => {
     return (
         <FooterArea>
-            ...
+            <div class="footer">
+                <p>Todos os direitos reservados</p>
+                <p>Copyright © 2022 Marcos Lamberti Company</p>
+                <p>Política de Privacidade</p>
+            </div>
         </FooterArea>
     );
 }
--- frontend/src/components/partials/Footer/styled.js
+++ frontend/src/components/partials/Footer/styled.js
@@ -3,8 +3,18 @@ import styled from 'styled-components';
 export const FooterArea = styled.div`
 
 
-    height: 100px;
-    background-color:#CCC;
+.footer{
+ 	border-top: 2px solid #888888;
+ 	padding: 20px 0;
+ 	text-align: center;
+ 	color: #888;
+ }
+
+ .footer p{
+ 	margin-top: 8px;
+ 	font-size: 14px;
+ 	text-align: center;
+ }
 
     
 `;
